5dff56896929686a3c695cd43f129d5e42e90e5d
Jose <86510147+jhlimardo@users.noreply.github.com>
2022-06-28 23:48:38 -0500

    First Commit -> Boiler plate create, and server test

---
+++ api/index.js
@@ -0,0 +1,3 @@
+const server = require('./src/app.js');
+
+server.listen(3001, () => console.log('Server running at port 3001'));
---
+++ api/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const server = express();
+
+server.name = 'API';
+
+server.get('/', (req, res) => {
+    res.send('Hola API Clientes')
+  })
+
+server.use(cors());
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
+
+// Error catching
+server.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
